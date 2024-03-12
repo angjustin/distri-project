3f426140819905ec83abcd8ae6b35c2417d03262
angjustin <angjustin425@gmail.com>
2024-03-12 16:43:37 +0800

    client and server connection

--- client/src/Client.java
+++ client/src/Client.java
@@ -1,15 +1,33 @@
+import java.io.*;
+import java.net.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Client {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    private static final String SERVER_IP = "127.0.0.1";
+    private static final int SERVER_PORT = 2222;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+        try (DatagramSocket socket = new DatagramSocket()) {
+            InetAddress serverAddress = InetAddress.getByName(SERVER_IP);
+
+            while (true) {
+                BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+                String message = userInput.readLine();
+                byte[] sendBuffer = message.getBytes();
+
+                DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, serverAddress, SERVER_PORT);
+                socket.send(sendPacket);
+
+                byte[] receiveBuffer = new byte[1024];
+                DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+                socket.receive(receivePacket);
+
+                String response = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                System.out.println("Response from server: " + response);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
--- server/src/Server.java
+++ server/src/Server.java
@@ -1,15 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.*;
+import java.net.*;
 public class Server {
+    private static final int PORT = 2222;
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        try (DatagramSocket socket = new DatagramSocket(PORT)) {
+            System.out.println("Server is running...");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            while (true) {
+                byte[] receiveBuffer = new byte[1024];
+                DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+                socket.receive(receivePacket);
+
+                String request = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                String response = handleRequest(request);
+
+                byte[] sendBuffer = response.getBytes();
+                DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, receivePacket.getAddress(), receivePacket.getPort());
+                socket.send(sendPacket);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    private static String handleRequest(String request) {
+        // Implement request handling logic here
+        System.out.println("request: " + request);
+        return "Response to " + request;
     }
 }
