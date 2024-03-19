737c30a67535005395d2f34f0b42010a44e835f8
CrazyCyfro <crazycyfro@gmail.com>
2024-03-19 10:39:15 +0800

    immutable, override equals and hashcode

--- client/src/Marshalling.java
+++ client/src/Marshalling.java
@@ -3,14 +3,14 @@ import java.util.Arrays;
 
 public class Marshalling {
     public static byte[] serialize(ReadRequest req) {
-        byte[] pathBytes = req.path.getBytes();
+        byte[] pathBytes = req.getPath().getBytes();
 
         ByteBuffer b = ByteBuffer.allocate(4);
         byte[] pathLengthBytes = b.putInt(0, pathBytes.length).array().clone();
-        byte[] offsetBytes = b.putInt(0, req.offset).array().clone();
-        byte[] lengthBytes = b.putInt(0, req.length).array().clone();
+        byte[] offsetBytes = b.putInt(0, req.getOffset()).array().clone();
+        byte[] lengthBytes = b.putInt(0, req.getLength()).array().clone();
 
-        byte[] output = new byte[pathBytes.length + 13];
+        byte[] output = new byte[pathBytes.length + 14];
 
         // 0 - ReadRequest, 1 - WriteRequest, 2 - MonitorRequest
         output[0] = 0;
@@ -18,6 +18,7 @@ public class Marshalling {
         System.arraycopy(pathBytes, 0, output, 5, pathBytes.length);
         System.arraycopy(offsetBytes, 0, output, 5 + pathBytes.length, 4);
         System.arraycopy(lengthBytes, 0, output, 9 + pathBytes.length, 4);
+        output[output.length - 1] = req.getId();
 
         return output;
     }
@@ -30,14 +31,14 @@ public class Marshalling {
 
         if (bytes[0] == 0) {
             // ReadRequest
-            ReadRequest req = new ReadRequest();
             int pathLength = ByteBuffer.wrap(bytes, 1, 4).getInt();
             byte[] pathBytes = Arrays.copyOfRange(bytes, 5, 5 + pathLength);
-            req.path = new String(pathBytes);
-            req.offset = ByteBuffer.wrap(bytes, 5 + pathLength, 4).getInt();
-            req.length = ByteBuffer.wrap(bytes, 9 + pathLength, 4).getInt();
+            String path = new String(pathBytes);
+            int offset = ByteBuffer.wrap(bytes, 5 + pathLength, 4).getInt();
+            int length = ByteBuffer.wrap(bytes, 9 + pathLength, 4).getInt();
+            byte id = bytes[bytes.length - 1];
 
-            return req;
+            return new ReadRequest(path, offset, length, id);
 
         } else {
             System.out.println("Request header invalid!");
--- client/src/ReadRequest.java
+++ client/src/ReadRequest.java
@@ -1,14 +1,60 @@
-public class ReadRequest {
-    public String path;
-    public int offset;
-    public int length;
+import java.util.Random;
 
-    public ReadRequest() {}
+public final class ReadRequest {
+    private final String path;
+    private final int offset;
+    private final int length;
+    private final byte id;
+
+    public String getPath() {
+        return path;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public byte getId() {
+        return id;
+    }
 
     public ReadRequest(String path, int offset, int length) {
         this.path = path;
         this.offset = offset;
         this.length = length;
+        this.id = (byte) ((new Random().nextInt() * offset + length) % 256 - 128);
+    }
+
+    public ReadRequest(String path, int offset, int length, byte id) {
+        this.path = path;
+        this.offset = offset;
+        this.length = length;
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+
+        if (!(o instanceof ReadRequest r)) {
+            return false;
+        }
+
+        return r.getPath().equals(path)
+                && r.getOffset() == offset
+                && r.getLength() == length
+                && r.getId() == id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id * path.length() * offset + length;
     }
 
     public void print() {
@@ -16,5 +62,6 @@ public class ReadRequest {
         System.out.println("Path: " + path);
         System.out.println("Offset: " + offset);
         System.out.println("Length: " + length);
+        System.out.println("ID: " + id);
     }
 }
