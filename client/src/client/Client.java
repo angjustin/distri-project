7fc440f489b384c56b8ea82130c681c8602d573d
cadyli <cady.li@gmail.com>
2024-03-26 12:18:34 +0800

    add choice of programme

--- client/src/client/Client.java
+++ client/src/client/Client.java
@@ -2,6 +2,7 @@ package client;
 
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -13,28 +14,54 @@ public class Client {
         try (DatagramSocket socket = new DatagramSocket()) {
             InetAddress serverAddress = InetAddress.getByName(SERVER_IP);
 
-            while (true) {
-                System.out.print("Enter file pathname: ");
-                BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
-                String filePath = userInput.readLine();
-                System.out.print("Enter offset (in bytes): ");
-                int offset = Integer.parseInt(userInput.readLine());
-                System.out.print("Enter number of bytes to read: ");
-                int bytesToRead = Integer.parseInt(userInput.readLine());
+            while (true){
+                // Display menu to choose the program
+                System.out.println("Choose the program: ");
+                System.out.println("1: Read content of a file by specifying the file pathname, offset, and number of bytes to read.");
+                System.out.println("2: Insert content into a file by specifying the file pathname, offset, and sequence of bytes.");
+                System.out.println("3: Monitor updates made to the content of a specified file for a designated time period.");
+                System.out.println("4: Terminate program.");
 
-                // Create ReadRequest object
-                ReadRequest readRequest = new ReadRequest(filePath,offset,bytesToRead);
-                // Serialize ReadRequest object
-                byte[] sendBuffer = Marshalling.serialize(readRequest);
-                DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, serverAddress, SERVER_PORT);
-                socket.send(sendPacket);
+                // Read user input
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter your choice: ");
+                int choice = scanner.nextInt();
 
-                byte[] receiveBuffer = new byte[1024];
-                DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
-                socket.receive(receivePacket);
+                // Process user choice
+                switch (choice) {
+                    case 1 -> {
+                        System.out.println("Service 1: Read content of a file by specifying pathname, offset, and number of bytes.");
+                        System.out.print("Enter file pathname: ");
+                        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+                        String filePath = userInput.readLine();
+                        System.out.print("Enter offset (in bytes): ");
+                        int offset = Integer.parseInt(userInput.readLine());
+                        System.out.print("Enter number of bytes to read: ");
+                        int bytesToRead = Integer.parseInt(userInput.readLine());
 
-                String response = new String(receivePacket.getData(), 0, receivePacket.getLength());
-                System.out.println("Response from server: " + response);
+                        // Create ReadRequest object
+                        ReadRequest readRequest = new ReadRequest(filePath, offset, bytesToRead);
+                        // Serialize ReadRequest object
+                        byte[] sendBuffer = Marshalling.serialize(readRequest);
+                        DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, serverAddress, SERVER_PORT);
+                        socket.send(sendPacket);
+                        byte[] receiveBuffer = new byte[1024];
+                        DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+                        socket.receive(receivePacket);
+                        String response = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                        System.out.println("Response from server: " + response);
+                    }
+                    case 2 ->
+                            System.out.println("Service 2: Insert content into a file by specifying pathname, offset, and sequence of bytes.");
+                    case 3 ->
+                            System.out.println("Service 3: Monitor updates made to a file's content for a designated time period.");
+                    case 4 -> {
+                        System.out.println("Terminating program.");
+                        return;
+                    }
+                    default -> System.out.println("Invalid choice. Please choose a valid option.");
+                }
+
             }
         } catch (IOException e) {
             e.printStackTrace();
